336a7444d0847476e8db9eefa544f094bbd42656
joanavrsilva <joanavrsilva@gmail.com>
2022-05-31 08:37:45 +0000

    Test heroku deployment

--- art_gallery/settings.py
+++ art_gallery/settings.py
@@ -19,7 +19,7 @@ if os.path.isfile("env.py"):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR = os.path.join(BASE_DIR. 'templates')
+TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'art_gallery',
-    'cloudinary';
+    'cloudinary',
     
 ]
 
